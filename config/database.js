bc526b44b63b18d1b608eb933471a89c6e281cf7
minhcasi <minhcasi@gmail.com>
2020-10-08 20:25:28 +0700

    update

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 // Default database config
-
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -15,43 +14,3 @@ module.exports = ({ env }) => ({
     },
   },
 });
-
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'postgres',
-//         host: env('DATABASE_HOST', 'database-strapi.c9y7r0bovjsx.ap-southeast-1.rds.amazonaws.com'),
-//         port: env.int('DATABASE_PORT', 5432),
-//         database: env('DATABASE_NAME', 'strapi'),
-//         username: env('DATABASE_USERNAME', 'inspireui'),
-//         password: env('DATABASE_PASSWORD', 'inspireui123'),
-//       },
-//       options: {
-//         ssl: false,
-//       },
-//     },
-//   },
-// });
-
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'postgres',
-//         host: env('DATABASE_HOST', '127.0.0.1'),
-//         port: env.int('DATABASE_PORT', 5432),
-//         database: env('DATABASE_NAME', 'strapi'),
-//         username: env('DATABASE_USERNAME', ''),
-//         password: env('DATABASE_PASSWORD', ''),
-//       },
-//       options: {
-//         ssl: false,
-//       },
-//     },
-//   },
-// });
